c44c379b8c44aa1ab022f613d365cc7d4906ae63
jcf5xh <jcf5xh@virginia.edu>
2014-03-21 03:17:35 -0400

    implemented both arrow and projectilefactory

---
+++ Retribution/ProjectileFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+{
+	public static class ProjectileFactory()
+	{
+        public ProjectileFactory()
+        {
+          
+        }
+        public void create(String s, GameObject start, GameObject obj)
+        {
+          if(s=="arrow")
+          {
+            Arrow arr=new Arrow(obj, start);
+              //need to add arrow to projectileManager
+          }
+        }
+
+
+
+	}
+}
